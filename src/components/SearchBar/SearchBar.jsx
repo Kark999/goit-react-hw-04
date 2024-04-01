fc6e94dbf2f7a80467676627cfb5208a2e2d96b9
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-02 01:37:51 +0300

    'update'

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,96 +1,49 @@
 import css from "./SearchBar.module.css";
-import { useFormik } from "formik";
+// import toast from "react-hot-toast";
+
 import * as Yup from "yup";
-import toast from "react-hot-toast";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const initialValues = { searchTerm: "" };
+
+const searchFormSchema = Yup.object().shape({
+  searchTerm: Yup.string().required("Search term is required"),
+});
 
 const SearchBar = ({ onsearchQuery }) => {
-  const formik = useFormik({
-    initialValues: {
-      searchTerm: "",
-    },
-    validationSchema: Yup.object({
-      searchTerm: Yup.string().required("Search term is required"),
-    }),
-    onSubmit: async (values) => {
-      try {
-        if (!values.searchTerm) {
-          toast.error("Please enter a search term");
-          return;
-        }
-
-        await onsearchQuery(values.searchTerm);
-      } catch (error) {
-        toast.error(error.message);
-      }
-    },
-  });
+  const handleSubmit = (values) => {
+    onsearchQuery(values.searchTerm);
+  };
 
   return (
     <header className={css.header}>
-      <form onSubmit={formik.handleSubmit} className={css.form}>
-        <input
-          className={css.field}
-          type="text"
-          name="searchTerm"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.searchTerm}
-        />
-        {formik.touched.searchTerm && formik.errors.searchTerm && (
-          <span className={css.error}>{formik.errors.searchTerm}</span>
-        )}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={searchFormSchema}
+      >
+        <Form className={css.form}>
+          <Field
+            className={css.field}
+            type="text"
+            name="searchTerm"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+          <ErrorMessage
+            className={css.error}
+            name="searchTerm"
+            component="span"
+          />
 
-        <button className={css.submitBtn} type="submit" aria-label="Search">
-          🔍
-        </button>
-      </form>
+          <button className={css.submitBtn} type="submit" aria-label="Search">
+            🔍
+          </button>
+        </Form>
+      </Formik>
     </header>
   );
 };
 
 export default SearchBar;
-
-// const SearchBar = ({ onSubmit, fetchPhotos }) => {
-//   const [searchTerm, setSearchTerm] = useState("");
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     if (!searchTerm.trim()) {
-//       toast.error("Please enter a search term.");
-//       return;
-//     }
-
-//     fetchPhotos(searchTerm);
-//   };
-
-//   const handleChange = (e) => {
-//     setSearchTerm(e.target.value);
-//   };
-
-//   return (
-//     <>
-//       <header className={css.header}>
-//         <form className={css.form} onSubmit={handleSubmit}>
-//           <input
-//             className={css.input}
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//             value={searchTerm}
-//             onChange={handleChange}
-//           />
-//           <button className={css.submitBtn} type="submit">
-//             🔍
-//           </button>
-//         </form>
-//       </header>
-//     </>
-//   );
-// };
-
-// export default SearchBar;
